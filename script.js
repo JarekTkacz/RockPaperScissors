dd16c8955c8ddf87605b3d640025bc821567f961
JarekTkacz <116753118+JarekTkacz@users.noreply.github.com>
2022-11-15 22:30:21 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 function check(){
 	  var user1 = prompt("User 1 please select: Rock, Paper or Scissors?");
 	  var user2 = prompt("User 2 please select: Rock, Paper or Scissors?");
-	  
-	  if(user1=="Paper" && user2=="Paper"){
-		  alert("It's a draw");
+	
+	if(user1!="Paper" && user1!="Rock" && user1!="Scissors"){
+		alert("Invalid input, please try again!");
+		}
+		else if(user2!="Paper" && user2!="Rock" && user2!="Scissors"){
+		  alert("Invalid input, please try again!");
 		  }
-			else if(user1=="Rock" && user2=="Rock"){
-			alert("It's a draw");
-			}
+	  
+	  			else if(user1=="Paper" && user2=="Paper"){
+		  		alert("It's a draw");
+		 	 	}
+				else if(user1=="Rock" && user2=="Rock"){
+				alert("It's a draw");
+				}
 				else if(user1=="Scissors" && user2=="Scissors"){
 				alert("It's a draw");
 				}
